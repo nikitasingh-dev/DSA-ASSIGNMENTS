dd4e3a7db1f4578ad78a4aa94cd11fd760151487
nikitasingh-dev <nikitasingh36821@gmail.com>
2025-11-06 12:00:01 +0530

    None

---
+++ ASSIGNMENT-1/Q-1.c
@@ -0,0 +1,25 @@
+// Question - 1
+
+// Find the second largest element in an array.
+
+#include <stdio.h>
+int main() {
+    int n,i,max1,max2;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter %d elements: ",n);
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    max1=max2=a[0];
+    for(i=1;i<n;i++){
+        if(a[i]>max1){
+            max2=max1;
+            max1=a[i];
+        } else if(a[i]>max2 && a[i]!=max1){
+            max2=a[i];
+        }
+    }
+    printf("Second largest element: %d",max2);
+    return 0;
+}
+
